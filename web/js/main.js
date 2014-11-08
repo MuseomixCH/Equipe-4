d3ec891dd938c01b1c4a0e4d32236a44be78154f
Daniel Huf <daniel.huf@swisscom.com>
2014-11-08 17:26:14 +0100

    maps, functions, files

--- web/js/main.js
+++ web/js/main.js
@@ -1,3 +1,32 @@
+/*
+    load floor image
+*/
+function loadFloor(floor=2){
+    $('#mainContent img').attr('src','img/maps/floor'+floor+'.svg');
+}
+
+/*
+    dot position in percent to image
+ */
+function drawDot(x,y,className='',name=''){
+    $('#floorImg').append('<div class="dot '+className+'" style="left:'+x+'%;top:'+y+'%">');
+}
+
+/*
+    beacon or photo
+ */
+
+function showButton(content='photo'){
+
+}
+
+/*
+    clean main content
+ */
+function hideMainContent(){
+    $('#mainContent').html('');
+}
+
 $( document ).ready(function() {
     var fun = "bja43whspny60x2,knliyajr8y6x0yt,xrhnpx6pfdlquob";
     var app = "bja43whspny60x2,knliyajr8y6x0yt";
@@ -13,4 +42,5 @@ $( document ).ready(function() {
         document.location = 'museochoix://loadContexts?ids='+idList;
         return false;
     });
+
 });
