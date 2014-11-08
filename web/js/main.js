faebe97fc529131c91676a180fa564d17cd13cda
Daniel Huf <daniel.huf@swisscom.com>
2014-11-08 15:25:13 +0100

    Change submit way

--- web/js/main.js
+++ web/js/main.js
@@ -1,20 +1,16 @@
 $( document ).ready(function() {
-    var fun = "bja43whspny60x2, knliyajr8y6x0yt, xrhnpx6pfdlquob";
-    var app = "bja43whspny60x2, knliyajr8y6x0yt";
+    var fun = "bja43whspny60x2,knliyajr8y6x0yt,xrhnpx6pfdlquob";
+    var app = "bja43whspny60x2,knliyajr8y6x0yt";
     var idList = '';
 
-    $('#formmuseochoix').submit(function() {
-
+    $('#formmuseochoix').submit(function(e) {
+        e.preventDefault();
         if( $('#facteurs .active input').val() == 'fun'){
             idList = fun;
         }else{
             idList = app;
         }
-
-        var newitem = document.createElement("input");
-            newitem.name = "ids";
-            newitem.type = "hidden";
-            newitem.value = idList;
-        $(this).append(newitem);
+        document.location = 'museochoix://loadContexts?ids='+idList;
+        return false;
     });
 });
