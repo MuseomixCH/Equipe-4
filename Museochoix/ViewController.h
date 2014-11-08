422b0d8d14b1ace513cfe018bfb6aee627e048c3
Mathieu Monney <zittix@xwaves.net>
2014-11-08 14:17:25 +0100

    - Initial commit of iOS template

---
+++ Museochoix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Museochoix
+//
+//  Created by Mathieu Monney on 08.11.14.
+//  Copyright (c) 2014 Vidinoti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Museochoix/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Museochoix
+//
+//  Created by Mathieu Monney on 08.11.14.
+//  Copyright (c) 2014 Vidinoti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
